30a9ce02167480b046add0edab3b0a77fb0c042b
jehron <jehronp@gmail.com>
2021-10-03 11:34:27 -0400

    My first commit!

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Skeleton code for the ColorStack Sprout Fall '21 pod project. PodSpace is a private social network for pods to stay connected with each other after the program.
 
 ## Collaborators
+
+Jehron Petty / CS @ Cornell '20
--- src/routes/HelloWorldRoute.ts
+++ src/routes/HelloWorldRoute.ts
@@ -19,7 +19,7 @@ export default class HelloWorldRoute extends BaseRoute<HelloWorldResult> {
   async content(): Promise<HelloWorldResult> {
     return {
       message: 'Looks like the server is up and running!',
-      podmates: []
+      podmates: ['Jehron Petty']
     };
   }
 }
