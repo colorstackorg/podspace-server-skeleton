997edd49142da3ef6ce6e801e1b88e51ec7313dd
Melissa Perez <melmperz@gmail.com>
2021-10-20 01:51:01 -0400

    Remove exampleField from setup comments

--- src/models/Reaction.ts
+++ src/models/Reaction.ts
@@ -46,9 +46,7 @@ const reactionSchema: Schema<ReactionDocument> = new Schema<ReactionDocument>(
 
     post: { ref: Model.POST, required: true, type: ID },
     type: { default: ReactionType.HEART, type: ReactionType },
-    user: { ref: Model.USER, required: true, type: ID },
-
-    exampleField: { required: true, type: String }
+    user: { ref: Model.USER, required: true, type: ID }
   },
   { timestamps: true }
 );
